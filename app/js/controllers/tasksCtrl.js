516672d7414420e27dc8329ef6cdb735bf2fb47c
Denis Zemlianoi <dzemlianoi@gmail.com>
2017-04-14 07:48:13 +0300

    tasks small redone

--- app/js/controllers/tasksCtrl.js
+++ app/js/controllers/tasksCtrl.js
@@ -1,10 +1,8 @@
-angular.module('todo').controller('tasksCtrl', ['$scope', '$http', function($scope, $http){
-    var request = {
-        method: 'GET',
-        url: "http://toodoo-rg.herokuapp.com/tasks",
-        data: { project_id: '????' }
-    };
-    $http(request).then(function (response) {
-        $scope.tasks = response.data;
-    });
-}]);
+angular.module('todo').controller('tasksCtrl', [
+    '$scope',
+    '$http',
+    'Task',
+    function($scope, $http, Task) {
+        $scope.tasks = Task.index({ project_id: '????' });
+    }
+]);
--- app/js/factories/task.js
+++ app/js/factories/task.js
@@ -1,8 +1,8 @@
 app = angular.module('todo');
 
 app.factory('Task', ['$resource', function($resource){
-    return $resource('/tasks/:id.json', { id: '@id' }, {
-        index:  { method: 'GET', isArray: true, responseType: 'json' },
+    return $resource('http://toodoo-rg.herokuapp.com/tasks/:id.json', { id: '@id' }, {
+        index:  { method: 'GET', responseType: 'json', isArray: true, params: { project_id: '@project_id' } },
         update: { method: 'PUT' },
         create: { method: 'POST' }
     })
