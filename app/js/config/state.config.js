7aaa0987f85b8bd5d23e6cb0f879fb1e5f815902
Denis Zemlianoi <dzemlianoi@gmail.com>
2017-04-14 14:19:11 +0300

    access modernized

--- app/js/config/state.config.js
+++ app/js/config/state.config.js
@@ -2,21 +2,39 @@ app = angular.module('todo');
 
 app.config(function($stateProvider, $urlRouterProvider) {
 
+    var unpermited = {
+        access: ['$auth', '$state', function($auth, $state){
+            $auth.validateUser().then(function() {
+                $state.go('home')
+            }, function(){
+                return true;
+            })
+        }]
+    };
+
     $urlRouterProvider.otherwise("/");
+
     $stateProvider
         .state('signin', {
-            url: "/signin",
+            url: "/",
             controller: 'signinCtrl',
-            templateUrl: "/js/views/signin.html"
+            templateUrl: "/js/views/signin.html",
+            resolve: unpermited
         })
         .state('signup', {
             url: "/signup",
             controller: 'signupCtrl',
-            templateUrl: "/js/views/signup.html"
+            templateUrl: "/js/views/signup.html",
+            resolve: unpermited
         })
         .state('home', {
-            url: "/",
+            url: "/todo",
             controller: 'projectsCtrl',
-            templateUrl: "/js/views/projects.html"
+            templateUrl: "/js/views/projects.html",
+            resolve: {
+                auth: function($auth) {
+                    return $auth.validateUser();
+                }
+            }
         })
 });
