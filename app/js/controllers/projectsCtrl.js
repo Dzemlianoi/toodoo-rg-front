50101eb52fc727d64bc365cc94e8ed4ccd46b85e
Denis Zemlianoi <dzemlianoi@gmail.com>
2017-04-14 07:45:42 +0300

    project factory and ctrl redone

--- app/js/controllers/projectsCtrl.js
+++ app/js/controllers/projectsCtrl.js
@@ -2,17 +2,21 @@ angular.module('todo').controller('projectsCtrl', [
     '$scope',
     '$http',
     'Project',
-    'Projects',
     function ($scope, $http, Project) {
         $scope.projects = Project.index();
-
         $scope.deleteProject = function (project) {
             Project.delete({ id: project.id }, function () {
                 $scope.projects = window._.without($scope.projects, project);
             })
         };
 
-        $scope.addProject = function (){
-            $scope.projects = $scope.projects.push(Project.create())
+        $scope.addProject = function () {
+            Project.create({}, function (project) {
+                $scope.projects.push(project);
+            })
+        };
+
+        $scope.renameProject = function() {
+
         }
 }]);
