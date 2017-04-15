52acf0a21cc2defeb91a48c38fc865d47097789e
Denis Zemlianoi <dzemlianoi@gmail.com>
2017-04-15 07:53:22 +0300

    renaming added

--- app/js/controllers/projectsCtrl.js
+++ app/js/controllers/projectsCtrl.js
@@ -2,13 +2,11 @@ angular.module('todo').controller('projectsCtrl', [
     '$scope',
     '$http',
     'Project',
-    function ($scope, $http, Project) {
+    'Task',
+    'Flash',
+    function ($scope, $http, Project, Task, Flash) {
+
         $scope.projects = Project.index();
-        $scope.deleteProject = function (project) {
-            Project.delete({ id: project.id }, function () {
-                $scope.projects = window._.without($scope.projects, project);
-            })
-        };
 
         $scope.addProject = function () {
             Project.create({}, function (project) {
@@ -16,7 +14,30 @@ angular.module('todo').controller('projectsCtrl', [
             })
         };
 
-        $scope.renameProject = function() {
+        $scope.editProject = function(project) {
+            $scope.edited_input = angular.element(document.querySelector('#project' + project.id + ' input'));
+            $scope.edited_input.removeAttr('readonly');
+            $scope.edited_input_title = project.title;
+        };
 
-        }
+        $scope.renameProject = function(project) {
+            Project.update({ id: project.id, title: project.title},
+                function(){
+                    if ($scope.edited_input){
+                        $scope.edited_input.attr('readonly', true);
+                        $scope.edited_input = '';
+                    }
+                    $scope.edited_input_title = '';
+                },
+                function(reason){
+                    if (reason.data.title){
+                        project.title = $scope.edited_input_title;
+                        Flash.create('danger', 'Title ' + reason.data.title.join('.<br/> Title '));
+                    }
+                })
+        };
+
+        $scope.renameProjectByEnter = function(project, keyEvent) {
+            if (keyEvent.which == 13) $scope.renameProject(project);
+        };
 }]);
