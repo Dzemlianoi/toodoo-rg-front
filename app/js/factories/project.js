888538938a2cf98f85eb8005f47ff112966953eb
Denis Zemlianoi <dzemlianoi@gmail.com>
2017-04-15 07:49:22 +0300

    for testing

--- app/js/factories/project.js
+++ app/js/factories/project.js
@@ -1,7 +1,7 @@
 app = angular.module('todo');
 
 app.factory('Project', ['$resource', function($resource){
-    return $resource('http://toodoo-rg.herokuapp.com/projects/:id.json', { id: '@id' }, {
+    return $resource('http://localhost:3000/projects/:id.json', { id: '@id' }, {
         index:  { method: 'GET', isArray: true, responseType: 'json' },
         update: { method: 'PUT' },
         create: { method: 'POST' }
