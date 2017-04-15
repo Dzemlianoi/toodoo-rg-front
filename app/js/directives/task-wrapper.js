901a181e572ac52d767a23cf0f151782748a1d88
Denis Zemlianoi <dzemlianoi@gmail.com>
2017-04-15 11:53:30 +0300

    fixed files

--- app/js/directives/task-wrapper.js
+++ app/js/directives/task-wrapper.js
@@ -3,10 +3,41 @@ app = angular.module('todo');
 app.directive('taskWrapper', function() {
     return {
         scope: {
-            'task': '='
+            'tasks': '=',
+            'project': '='
         },
         templateUrl: 'js/views/task-wrapper.html',
-        controller: ['$scope', 'Task', function ($scope, Task) {
+        controller: ['$scope', 'Task', 'Flash', function ($scope, Task, Flash) {
+            $scope.addTask = function(){
+                Task.create({ project_id: $scope.project.id, title: $scope.task_title },
+                    function(response){
+                        $scope.tasks.push(response);
+                        $scope.task_title = '';
+                    },
+                    function(reason) {
+                        if (reason.data.title){
+                            Flash.create('danger', 'Title ' + reason.data.title.join('.<br/> Title '));
+                        }
+                    }
+                )
+            };
+
+            $scope.removeTask = function(task){
+                Task.delete({ id: task.id }, function(){
+                    $scope.tasks = window._.without($scope.tasks, task);
+                })
+            };
+
+            $scope.updateComplitiion = function(task){
+                Task.update({ id: task.id, completed: task.completed }, function(response){
+                    task = response;
+                })
+            };
+
+            $scope.addTaskByEnter = function(task, keyEvent) {
+                console.log(keyEvent);
+                if (keyEvent.which == 13) $scope.addTask(task);
+            };
         }]
     }
 });
