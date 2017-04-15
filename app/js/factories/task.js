08b2e2c52a1124dcaf8d4f76f55ceb8bd56896f1
Denis Zemlianoi <dzemlianoi@gmail.com>
2017-04-15 10:13:36 +0300

    test on herokuapp

--- app/js/factories/task.js
+++ app/js/factories/task.js
@@ -1,7 +1,7 @@
 app = angular.module('todo');
 
 app.factory('Task', ['$resource', function($resource){
-    return $resource('http://toodoo-rg.herokuapp.com/tasks/:id.json', { id: '@id' }, {
+    return $resource('http://localhost:3000/tasks/:id.json', { id: '@id' }, {
         index:  { method: 'GET', responseType: 'json', isArray: true, params: { project_id: '@project_id' } },
         update: { method: 'PUT' },
         create: { method: 'POST' }
