b4f70413703dd9642a598f5114f9b290cbcda146
Denis Zemlianoi <dzemlianoi@gmail.com>
2017-04-16 07:50:05 +0300

    orderUp and orderDown

--- app/js/factories/task.js
+++ app/js/factories/task.js
@@ -4,6 +4,8 @@ app.factory('Task', ['$resource', function($resource){
     return $resource('http://localhost:3000/tasks/:id.json', { id: '@id' }, {
         index:  { method: 'GET', responseType: 'json', isArray: true, params: { project_id: '@project_id' } },
         update: { method: 'PUT' },
-        create: { method: 'POST' }
+        create: { method: 'POST' },
+        orderUp: { method: 'PATCH', responseType: 'json', params: { task_id: '@task_id' }  },
+        orderDown: { method: 'PATCH', responseType: 'json', params: { task_id: '@task_id' }  }
     })
 }]);
